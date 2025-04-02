a6eb8cbb336fb7f20493037a16977d86c1e3ea22
AlphaIsYour <alphrenoorz@gmail.com>
2025-04-02 14:50:43 +0700

    Initial commit: Setup proyek SmartWaste GIS

---
+++ components/LeafletMap.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="map" class="map"></div>
+</template>
+
+<script>
+import { onMounted } from "vue";
+
+export default {
+  name: "LeafletMap",
+  setup() {
+    onMounted(() => {
+      if (import.meta.client) {
+        // Pastiin cuma jalan di client
+        import("leaflet").then((L) => {
+          import("leaflet/dist/leaflet.css"); // CSS-nya juga di-import dinamis
+          const map = L.map("map").setView([-7.9535, 112.6145], 13); // Koordinat contoh
+          L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+            attribution:
+              '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+          }).addTo(map);
+        });
+      }
+    });
+  },
+};
+</script>
+
+<style scoped>
+.map {
+  width: 100%;
+  height: 400px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <h1>SmartWaste GIS</h1>
+    <ClientOnly>
+      <LeafletMap />
+    </ClientOnly>
+  </div>
+</template>
+
+<script>
+import LeafletMap from "../components/LeafletMap.vue";
+export default {
+  name: "HomePage",
+  components: {
+    LeafletMap,
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  text-align: center;
+  padding: 20px;
+}
+</style>
